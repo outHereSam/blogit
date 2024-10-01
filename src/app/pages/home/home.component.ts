775fbed9ed302e0e4b115b8d835f925934ef6aa4
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-10-01 14:58:30 +0000

    Implements sign up

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.sass',
 })
 export class HomeComponent {
+  constructor(private authService: AuthService) {}
 
+  onLogout() {
+    this.authService.logout();
+  }
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.sass'
+  styleUrl: './signup.component.sass',
 })
 export class SignupComponent {
+  signupForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    { validators: this.checkPasswordsMatch }
+  );
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  checkPasswordsMatch(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notMatch: true };
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      const { email, username, password } = this.signupForm.value;
+      if (email && username && password) {
+        this.authService
+          .signup(email, username, password)
+          .then(() => {
+            this.router.navigate(['login']);
+          })
+          .catch((err) => console.error('Signup failed', err.message));
+      }
+    } else {
+      console.log(this.signupForm.errors);
+      this.signupForm.markAllAsTouched();
+    }
+  }
 }
