b01986da8f30cb362112114bcf1aa45d4c9b0785
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-10-01 09:25:53 +0000

    Adds and configures firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAERQ6xAynltP3ajChJVdslrHrynT3Ldbc',
+    authDomain: 'blogit-d82c9.firebaseapp.com',
+    projectId: 'blogit-d82c9',
+    storageBucket: 'blogit-d82c9.appspot.com',
+    messagingSenderId: '699381726760',
+    appId: '1:699381726760:web:4215fce90e000bd157ff1d',
+    measurementId: 'G-DQP3QYK6LN',
+  },
+};
