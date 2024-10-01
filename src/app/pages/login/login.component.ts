04d335642375b24ef114c7c416021ec36f463320
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-10-01 14:39:49 +0000

    Implements user login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass'
+  styleUrl: './login.component.sass',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      if (email && password) {
+        this.authService
+          .login(email, password)
+          .then(() => {
+            this.router.navigate(['/']);
+          })
+          .catch((err) => console.error('Login failed', err.message));
+      }
+    } else {
+      console.log(this.loginForm.errors);
+    }
+  }
 }
