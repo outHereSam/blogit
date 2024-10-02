bc35b327f3a42a5d7ddbfd26b16cc5cb12ce202e
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-10-02 16:39:07 +0000

    Adds loader for loading states

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,17 +10,20 @@ import { Router, RouterLink } from '@angular/router';
 
 import { NOTYF } from '../../../utils/notyf.token';
 import { Notyf } from 'notyf';
+import { LoaderComponent } from '../../components/loader/loader.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, LoaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass',
 })
 export class LoginComponent {
   successMessage!: string;
   error!: string;
+  isLoading: boolean = false;
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
@@ -34,16 +37,20 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
+      this.isLoading = true;
+
       const { email, password } = this.loginForm.value;
       if (email && password) {
         this.authService
           .login(email, password)
           .then(() => {
+            this.isLoading = false;
             this.successMessage = 'Login Successful';
             this.notyf.success(this.successMessage);
             this.router.navigate(['/']);
           })
           .catch((err) => {
+            this.isLoading = false;
             this.error = 'Login failed. Invalid credentials';
             this.notyf.error(this.error);
             console.error('Login failed', err.message);
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -10,17 +10,19 @@ import { AuthService } from '../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
 import { NOTYF } from '../../../utils/notyf.token';
 import { Notyf } from 'notyf';
+import { LoaderComponent } from '../../components/loader/loader.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, LoaderComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.sass',
 })
 export class SignupComponent {
   successMessage!: string;
   error!: string;
+  isLoading: boolean = false;
 
   signupForm = new FormGroup(
     {
@@ -53,16 +55,19 @@ export class SignupComponent {
 
   onSubmit() {
     if (this.signupForm.valid) {
+      this.isLoading = true;
       const { email, username, password } = this.signupForm.value;
       if (email && username && password) {
         this.authService
           .signup(email, username, password)
           .then(() => {
+            this.isLoading = false;
             this.successMessage = 'Account Created Successfully';
             this.notyf.success(this.successMessage);
             this.router.navigate(['/login']);
           })
           .catch((err) => {
+            this.isLoading = false;
             this.error = 'Failed to create account';
             this.notyf.error(this.error);
             console.error('Signup failed', err.message);
